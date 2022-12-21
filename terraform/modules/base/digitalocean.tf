90ab79d9551e4a9f49a70dabc2724001c964a1ae
sal <ximena.salgadouribe@epfl.ch>
2022-12-21 15:35:28 +0100

    fix: drop deprecated template provider

--- terraform/modules/base/digitalocean.tf
+++ terraform/modules/base/digitalocean.tf
@@ -3,13 +3,12 @@ resource "digitalocean_ssh_key" "ssh_key" {
   public_key = tls_private_key.ssh_key.public_key_openssh
 }
 
-data "template_file" "cloud-init-yaml" {
-  template = file("${path.module}/files/cloud-init.yml")
-  vars = {
+locals {
+  cloud_init_yml = templatefile("${path.module}/files/cloud-init.yml", {
     user                   = var.droplet_user
     init_ssh_public_key    = tls_private_key.ssh_key.public_key_openssh
     docker_compose_version = var.docker_compose_version
-  }
+  })
 }
 
 resource "digitalocean_domain" "domain" {
